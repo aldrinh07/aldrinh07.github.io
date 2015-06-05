6463f59b598e6907a47158614549f4f4aa0f9233
aldrinhernando <aldrinh@live.com>
2015-06-05 15:59:35 -0600

    initial push and test

---
+++ js/mainCtrl.js
@@ -0,0 +1,25 @@
+var app = angular.module('myApp');
+
+
+app.controller('mainCtrl', function($scope){
+
+    $scope.test = "test worked";
+
+    $scope.album1 = function(){
+        $scope.showalbum1 = true;
+        $scope.showalbum2 = false;
+    }
+
+    $scope.album2 = function(){
+        $scope.showalbum1 = false;
+        $scope.showalbum2 = true;
+    }
+
+
+
+
+
+
+
+
+});
